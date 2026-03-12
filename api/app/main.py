6208a84e1d7d67795310f66c8e55ac6f57e499d7
noahdavis05 <sc23nd@leeds.ac.uk>
2026-03-12 11:50:27 +0000

    Improved docs, and added documentation directory with pdf docs

--- api/app/main.py
+++ api/app/main.py
@@ -75,15 +75,19 @@ app = FastAPI(
     description="This API find the cheapest journey between UK cities via public transport. This combines national rail services, FlixBus coaches, and National Express coaches.",
     openapi_tags=tags_metadata
 )
+
+# This block is only used when running locally and we want to run
+# rapipdf JS to convert our api into PDF. it stops CORS issues.
+"""
 from fastapi.middleware.cors import CORSMiddleware
-# Add this block!
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # This allows ANY site (including your local file) to access the API
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+"""
 
 app.include_router(routes.router)
 app.include_router(cities.router)
